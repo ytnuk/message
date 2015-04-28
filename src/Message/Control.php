7b1374d8f5de0770344694c2e5f32523f2b537e0
Vít Kutný <vitkutny@icloud.com>
2015-04-28 23:19:26 +0200

    dev

--- src/Message/Control.php
+++ src/Message/Control.php
@@ -15,7 +15,6 @@ final class Control extends Ytnuk\Application\Control
 
 	protected function startup()
 	{
-		//TODO: use Flash/Message storage when available
 		$parent = $this->lookup(Nette\Application\UI\Control::class);
 		$parameters = $parent->getTemplate()->getParameters();
 		$this->getTemplate()->add('messages', isset($parameters['flashes']) ? $parameters['flashes'] : []);
