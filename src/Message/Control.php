1043e4f4b55b9a0cf494e604c71c7d9b2df95429
Vít Kutný <vitkutny@icloud.com>
2015-06-18 22:55:13 +0200

    dev

--- src/Message/Control.php
+++ src/Message/Control.php
@@ -13,10 +13,15 @@ use Ytnuk;
 final class Control extends Ytnuk\Application\Control
 {
 
+	/**
+	 * @return array
+	 */
 	protected function startup()
 	{
 		$parent = $this->lookup(Nette\Application\UI\Control::class);
 		$parameters = $parent->getTemplate()->getParameters();
-		$this->getTemplate()->add('messages', isset($parameters['flashes']) ? $parameters['flashes'] : []);
+		return [
+			'messages' => isset($parameters['flashes']) ? $parameters['flashes'] : []
+		];
 	}
 }
